0d24128098e74627af89517d82403589b1010401
SECRETMAN5 <108389518+SECRETMAN5@users.noreply.github.com>
2022-11-17 10:14:52 +0400

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -55,7 +55,7 @@ def bul(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"☑️ **Mahnı adı**: [{title[:35]}]({link})\n🎬 **Mənbə**: YouTube\n⏱️ **Müddət**: `{duration}`\n👁‍🗨 **Baxış sayı**: `{views}`\n📤 **ᴍᴜsɪᴄ ᴋᴀɴᴀʟɪᴍ**: @UIviMusic"
+        rep = f"☑️ **Mahnı adı**: [{title[:35]}]({link})\n🎬 **Mənbə**: YouTube\n⏱️ **Müddət**: `{duration}`\n👁‍🗨 **Baxış sayı**: `{views}`\n📤 **Musiqi Kanalım**: @bossplaylistaz"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
