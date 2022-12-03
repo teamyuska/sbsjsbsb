b8fc06314c3b29f42fe4f029c631c89fe57f3f2a
teamyuska <118346503+teamyuska@users.noreply.github.com>
2022-12-03 10:13:18 +0400

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ que = {}
 admins = {}
 
 SESSION_NAME = getenv("SESSION_NAME", "AgAQ0zM39w-pQD7GcvgV9ZbBe_2GBLQTPAzLf_8UQrlAEJCZx_R_N7lXghw_bjl1sf8j5wx8o-m9iJkz0E4IvLaDvwVvLhkcovATL0a-dG89dAy33w2QWu_GKVrpnER0QYBqr3loUbl699HQi_PibWgaOG2ZxEWM08-eALUHf5ko5WTnYEKMdoCzoHgorKbBdi_3uP57edApEuEreiZpLRBCtzhkPC8hnOSRKQ7nslp3Ev28neZrUsSxQD-tnhfTsOI2_A8ifhSKV6u76jnila0bX-UOEL6LeXSRf8LpfQi7qYhDtiBIBxJcjSTqRgDQNslhffWeaeoyYv4zE96cupLTAAAAAUSeCNcA")
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("BOT_TOKEN", "5510964177:AAGkFJSZMcHmV0cpI1M-J1zhF5VCMIB7YuA")
 BOT_NAME = getenv("BOT_NAME", "Rahid_Asistan") 
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
