6c31556c8d990fcb13bd40ceb6ebb0a7f5a04db9
teamyuska <118346503+teamyuska@users.noreply.github.com>
2022-12-03 10:18:20 +0400

    Update config.py

--- config.py
+++ config.py
@@ -16,4 +16,4 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "2126111892").split()))
